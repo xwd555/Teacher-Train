2141e9c0ccb12f909dd66a0c099e515d8895b97f
xwd555 <1046836059@qq.com>
2019-12-12 22:08:43 +0800

    Revert "test"
    This reverts commit 39a976c7

---
+++ src/views/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+<script  lang="ts">
+let mybirth: number = 6
+console.log(mybirth)
+</script>
+<style lang="scss">
+  $primary-color: #ffe174;
+  body{
+    background-color: $primary_color;
+  }
+</style>
